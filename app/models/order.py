87f2561d644d9cd2b83ba164c3b726eec80ee22d
SarthakSuthar <sarthak.rockersinfo@gmail.com>
2026-07-20 15:44:40 +0530

    sqlalchemy models added

---
+++ app/models/order.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+from decimal import Decimal
+import uuid
+
+from sqlalchemy import DateTime, ForeignKey, Numeric, String, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.models.base import Base
+from app.models.user import User
+
+
+class Order(Base):
+    __tablename__ = "orders"
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.UUID )
+    user: Mapped["User"] = relationship(back_populates="orders")
+    user_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("users.id", ondelete="CASCADE"), index=True)
+    customer_name: Mapped[str] = mapped_column(String(255))
+    total_amount: Mapped[Decimal] = mapped_column(Numeric(10,2), default=0.00)
+    notes: Mapped[list["NotesItem"]] = relationship(back_populates="order", cascade="all, delete-orphan")
+    line_items : Mapped[list["OrderLineItem"]] = relationship(back_populates="order", cascade="all, delete-orphan"
+    )
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), 
+        server_default=func.now(), 
+        onupdate=func.now()
+    )
+
+class OrderLineItem:
+    __tablename__ = "order_line_items"
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.UUID )
+    order_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("orders.id", ondelete="CASCADE"), index=True)
+    item_name: Mapped[str] = mapped_column(String(255))
+    measurement: Mapped[str] = mapped_column(String(60))
+    quantity: Mapped[Decimal] = mapped_column(Numeric(10, 2))
+    rate: Mapped[Decimal] = mapped_column(Numeric(10, 2))
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    order: Mapped["Order"] = relationship(back_populates="line_items")
+
+
+class NotesItem:
+    __tablename__ = "notes_items"
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.UUID )
+    order_id: Mapped[uuid.UUID] = mapped_column(ForeignKey("orders.id", ondelete="CASCADE"), index=True)
+    note: Mapped[str] = mapped_column(String(500))
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now())
+    order: Mapped["Order"] = relationship(back_populates="line_items")
---
+++ app/models/user.py
@@ -0,0 +1,22 @@
+from app.models.base import Base
+import uuid
+from datetime import datetime
+from sqlalchemy import String,  DateTime
+from sqlalchemy.sql import func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from app.models.order import Order
+
+class User(Base):
+    __tablename__ = "users"
+    id: Mapped[uuid.UUID] = mapped_column(primary_key=True, default=uuid.UUID )
+    google_id: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    email: Mapped[str] = mapped_column(String(255), unique=True, index=True)
+    user_name:Mapped[str] = mapped_column(String(255))
+    company_name: Mapped[str | None] = mapped_column(String(255))
+    contact_person_name: Mapped[str | None] = mapped_column(String(255))
+    contact_number: Mapped[str | None] = mapped_column(String(255))
+    address: Mapped[str | None] = mapped_column(String(255))
+    orders: Mapped[list["Order"]]= relationship(back_populates="user", cascade="all, delete-orphan")
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone= True), server_default=func.now())
+    updated_at: Mapped[datetime] = mapped_column(DateTime(timezone= True), server_default=func.now(), onupdate= func.now())
